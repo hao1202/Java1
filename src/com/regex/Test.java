dcab0970c77f69b0a0349874cee2ea2575288b22
Tran Van Hao <tranvanhao2122003@gmail.com>
2023-06-20 11:53:49 +0700

    update package regex

--- src/com/regex/Test.java
+++ src/com/regex/Test.java
@@ -1,13 +1,13 @@
 package com.regex;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Test {
     public static void main(String[] args) {
+        BufferedReader bufferedReader = null;
         try {
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
             System.out.println("Nhap so dien thoai : ");
             String phone = bufferedReader.readLine();
             System.out.println(Regex.regexPhone(phone));
@@ -16,6 +16,14 @@ public class Test {
             System.out.println(Regex.regexEmail(email));
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
         System.out.println();
     }
